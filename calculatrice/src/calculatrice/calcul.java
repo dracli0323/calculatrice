5d8366e57d7ae8c15a5e5fe880fae747d75b2ee4
Axel-Murat <123635173+Axel-Murat@users.noreply.github.com>
2023-10-06 15:43:46 +0200

    rajout du tokken en commentaire

--- calculatrice/src/calculatrice/calcul.java
+++ calculatrice/src/calculatrice/calcul.java
@@ -3,5 +3,8 @@ package calculatrice;
 public class calcul {
 public static void main (String[] args) {
 	System.out.println("Hello World");
-}
+	}
+
+/* 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 */
+
 }
