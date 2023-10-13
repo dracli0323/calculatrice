496241f4202a4e7e3095f5ca1831233527d5c189
Axel-Murat <123635173+Axel-Murat@users.noreply.github.com>
2023-10-13 14:38:45 +0200

    Rajout de l'interface

---
+++ calculatrice/src/calculatrice/CalculatorModel.java
@@ -0,0 +1,5 @@
+package calculatrice;
+
+public interface CalculatorModel {
+
+}
---
+++ calculatrice/src/calculatrice/CalculatorModelInterface.java
@@ -0,0 +1,36 @@
+package calculatrice;
+
+public class CalculatorModelInterface implements CalculatorModel {
+	public void add() {
+	}
+
+	public void substract() {
+	}
+	
+	public void multiply() {
+	}
+	
+	public void divide() {
+	}
+	
+	public void opposite() {
+	}
+	
+	public void push() {
+	}
+	
+	public void pop() {
+	}
+	
+	public void drop() {
+	}
+	
+	public void swap() {
+	}
+	
+	public void clear() {
+	}
+	
+	
+	
+}
