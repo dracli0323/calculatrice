7bf11d678e24eee73ebd2e72c49d28b329b25041
marca <marca@LenovoMA3>
2023-10-13 17:40:12 +0200

    début controler

---
+++ calculatrice/src/calculatrice/CalculatorControler.java
@@ -0,0 +1,13 @@
+package calculatrice;
+
+import java.util.List;
+
+public class CalculatorControler implements CalculatorControlerInterface {
+	
+	public void change(String a) {
+		
+	}
+	public void change(List<Double> a) {
+
+}
+}
---
+++ calculatrice/src/calculatrice/CalculatorControlerInterface.java
@@ -0,0 +1,9 @@
+package calculatrice;
+
+import java.util.List;
+
+public interface CalculatorControlerInterface {
+	public void change(String a);
+	
+	public void change(List<Double> a);
+}
