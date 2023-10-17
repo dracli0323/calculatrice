4dc6df7bca4457e02eb375f1a2ee13baa4052c34
marca <marca@LenovoMA3>
2023-10-17 17:38:01 +0200

    token en commentaire2

--- calculatrice/src/calculatrice/calcul.java
+++ calculatrice/src/calculatrice/calcul.java
@@ -10,5 +10,5 @@ public static void main (String[] args) {
 
 
 /* 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 */
-
+/*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*/
 }
